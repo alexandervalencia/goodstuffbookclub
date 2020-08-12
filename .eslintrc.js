e139292e054ad5a1f7d41aa5fdae8992c2f1e28b
Alexander Valencia <alex.valencia@me.com>
2020-08-12 12:35:34 -0500

    feat: add tailwindcss, eslint, & stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2020: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'prettier/react'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
